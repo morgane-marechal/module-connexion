f27350c42756a1f0d8baf0d44c0f1145797b1989
morgane-marechal <morgane.marechal@laplateforme.io>
2022-11-28 14:00:18 +0100

    ajout module connexion.php

--- inscription.php
+++ inscription.php
@@ -1,3 +1,6 @@
+<?php
+include 'dbconnect.php';
+?>
 <head>
     <meta charset="utf-8"/>
     <link rel="stylesheet" type="text/css" 
@@ -25,21 +28,13 @@
     </header>
     <body>
 
-    <?php
-        $mysqli = new mysqli('localhost','root','','utilisateurs'); /*connexion avec la base de donnée -> rajouter de nouveau utilisateurs*/
-          
-        if($connection->connect_error){
-            die('Erreur : ' .$connection->connect_error);
-        }
-        echo 'Connexion réussie'; /*vérifie connexion avec base de donnée*/    
-    ?> 
     <section id="compte_form">
-        <form method="post" action="traitement.php">
+        <form action="" method="post">
             <h3>Création de compte</h3>
             <div id="fullname">
                 <input type="text" name="login" id="name" placeholder="Login*" required maxlength="255">
-                <input type="text" name="name" id="name" placeholder="Nom*" required maxlength="255">
-                <input type="text" name="firstname" id="firstname" placeholder="Prénom*" required maxlength="255">
+                <input type="text" name="nom" id="name" placeholder="Nom*" required maxlength="255">
+                <input type="text" name="prenom" id="firstname" placeholder="Prénom*" required maxlength="255">
             </div>
             <input type="password" name="password" id="password" placeholder="Password*" required maxlength="255">
             <input type="password" name="conf_password" id="conf_password" placeholder="Confirmation du mot de passe*" required maxlength="255">
